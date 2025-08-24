8a60fed0acba9d19c1c88f91f89a9cea26a95b35
Emanuel0112 <emanueldospassos.12@gmail.com>
2025-08-24 15:27:26 -0300

    Edição na inteiração da pagina.

--- MauiAppMinhaCompras/App.xaml.cs
+++ MauiAppMinhaCompras/App.xaml.cs
@@ -1,10 +1,38 @@
-﻿namespace MauiAppMinhaCompras
+﻿using MauiAppMinhaCompras.Helpers;
+
+namespace MauiAppMinhaCompras
 {
     public partial class App : Application
     {
+        static SQliteDatabaseHelper _db;
+
+        public static SQliteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null) 
+                {
+                    string path = Path.Combine
+                        (
+                        Environment.GetFolderPath
+                        (
+                            Environment.SpecialFolder.LocalApplicationData
+                            ),
+                        "banco_splite_compras.db3"  
+
+                        ); 
+                    _db = new SQliteDatabaseHelper(path);
+                }
+               
+
+                return _db; 
+            }
+        
+        }
         public App()
         {
             InitializeComponent();
+    
 
             //MainPage = new AppShell();
 
